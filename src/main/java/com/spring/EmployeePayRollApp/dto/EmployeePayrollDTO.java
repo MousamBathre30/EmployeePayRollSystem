8a891bc3e3b907585c60aecdecd4c6ce37ab5b9c
Mousam bathre <bathremousam@gmail.com>
2025-02-24 15:03:05 +0530

    [Mousam Bathre][adding the dto class to the uc 1 ]

---
+++ src/main/java/com/spring/EmployeePayRollApp/dto/EmployeePayrollDTO.java
@@ -0,0 +1,14 @@
+package com.spring.EmployeePayRollApp.dto;
+
+public class EmployeePayrollDTO {
+    public String name;
+    public long salary;
+
+    public EmployeePayrollDTO(String name,long salary){
+        this.name = name;
+        this.salary = salary;
+    }
+
+
+
+}
