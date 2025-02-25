2fea8251d2ef929541830b4531c7ab0e677b6907
Mousam bathre <bathremousam@gmail.com>
2025-02-25 11:59:18 +0530

    [Mousam bathre] [adding log method to the main file]

--- src/main/java/com/spring/EmployeePayRollApp/EmployeePayRollAppApplication.java
+++ src/main/java/com/spring/EmployeePayRollApp/EmployeePayRollAppApplication.java
@@ -1,13 +1,21 @@
 package com.spring.EmployeePayRollApp;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationContext;
+
 @SpringBootApplication
+@Slf4j
 public class EmployeePayRollAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayRollAppApplication.class, args);
+
+		ApplicationContext context = SpringApplication.run(EmployeePayRollAppApplication.class, args);
+		log.info("Employee Payroll App Started in Environment" , context.getEnvironment().getProperty("environment"));
+        log.info("Employee Payroll DB User is {} ", context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 
 }
