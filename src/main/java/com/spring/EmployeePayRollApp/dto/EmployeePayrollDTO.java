e2e4d5c72e22790bea2cc31dd95361e9f255965f
Mousam bathre <bathremousam@gmail.com>
2025-02-25 12:45:15 +0530

    [Mousam Bathre] adding the validation feature

--- src/main/java/com/spring/EmployeePayRollApp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/spring/EmployeePayRollApp/dto/EmployeePayrollDTO.java
@@ -1,14 +1,27 @@
 package com.spring.EmployeePayRollApp.dto;
 
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+
 public class EmployeePayrollDTO {
+    @NotEmpty(message = "Employee name cannot be null")
+    @Pattern(regexp = "^[A-Z][a-zA-Z\\s]{2,}$", message = "Employee name Invalid")
     public String name;
+
+    @Min(value = 500 , message = "Min Wage should be more than 500")
     public long salary;
+
 
     public EmployeePayrollDTO(String name,long salary){
         this.name = name;
         this.salary = salary;
     }
 
-
+    @Override
+    public String toString(){
+        return "name=" + name + ": Salary="+salary;
+    }
 
 }
